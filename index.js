ab11e6a72e63ad99430f874e56e61233fddc829e
Rjkfreelance <phpteach@outlook.com>
2020-10-18 19:41:58 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('/', (req, res) =>
 
   <script src='https://cdn.jsdelivr.net/gh/phoboslab/jsmpeg@9cf21d3/jsmpeg.min.js'></script>
   <script>
-    new JSMpeg.Player('wss://' + location.host + '/api/stream1', {
+    new JSMpeg.Player('ws://' + location.host + '/api/stream1', {
       canvas: document.getElementById('canvas1')
       
     })
